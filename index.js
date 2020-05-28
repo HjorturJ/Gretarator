c5c0aa93481dfa6f73521e28484f06d669571fbd
HjorturJ <hjorturv17@ru.is>
2020-05-28 17:30:25 +0000

    set header option for post (and more) operations

--- index.js
+++ index.js
@@ -8,36 +8,37 @@ const path = require('path');
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
 });
 
 //HTML random nickname site
-app.get('/', async function(req, res) {
-    return res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/', async function (req, res) {
+    return res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 //http://localhost:3000/api/nicknames [GET]
-app.get('/api/nicknames', async function(req, res) {
+app.get('/api/nicknames', async function (req, res) {
     const result = await nicknameService.getAllNicks();
     return res.status(result.status).json(result.body);
 });
 
 //http://localhost:3000/api/nicknames/random [GET]
-app.get('/api/nicknames/random', async function(req, res) {
+app.get('/api/nicknames/random', async function (req, res) {
     const result = await nicknameService.getRandomNick();
     return res.status(result.status).json(result.body);
 });
 
 //http://localhost:3000/api/nicknames/:nick [GET]
-app.get('/api/nicknames/:nick', async function(req, res) {
+app.get('/api/nicknames/:nick', async function (req, res) {
     const nick = req.params.nick;
     const result = await nicknameService.getNickByName(nick);
     return res.status(result.status).json(result.body);
 });
 
 //http://localhost:3000/api/nicknames/:nick [PUT]
-app.put('/api/nicknames/:nick', async function(req, res) {
+app.put('/api/nicknames/:nick', async function (req, res) {
     //Parameters in the URL are in UTF-8 unicode format that we need to decode.
     const nick = decodeURIComponent(escape(req.params.nick));
     const nickInput = req.body;
@@ -46,19 +47,19 @@ app.put('/api/nicknames/:nick', async function(req, res) {
 });
 
 //http://localhost:3000/api/nicknames/:nick [DELETE]
-app.delete('/api/nicknames/:nick', async function(req, res) {
+app.delete('/api/nicknames/:nick', async function (req, res) {
     const nick = req.params.nick;
     const result = await nicknameService.deleteNickByName(nick);
     return res.status(result.status).json(result.body);
 });
 
 //http://localhost:3000/api/nicknames [POST]
-app.post('/api/nicknames', async function(req, res) {
+app.post('/api/nicknames', async function (req, res) {
     const result = await nicknameService.createNick(req.body);
     return res.status(result.status).json(result.body);
 });
 
 //http://localhost:3000
-app.listen(port, function() {
+app.listen(port, function () {
     console.log(`Server is listening on port ${port}`);
 });
