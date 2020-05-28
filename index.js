da685f9abde9264fcbb0d1ec0e2fb14bb9e3c617
HjorturJ <hjorturv17@ru.is>
2020-05-28 17:40:45 +0000

    had the wrong header in the methods header section

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Origin', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
 });
 
